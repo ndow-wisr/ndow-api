58d53d1bff697c1a5d4a72d60c224dfa64bb984f
kissmygritts <kissmygritts08@gmail.com>
2016-12-06 09:38:38 -0800

    animal api resource pending implementation

--- routes/animals.js
+++ routes/animals.js
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
         attributes: ['id', 'loc_name', 'loc_lat', 'loc_lon'] // TODO: once mountain range and hunt unit data is incorporated include those data here.
       }, {
         model: models.Project,
-        attributes: ['id', 'proj_name']
+        attributes: ['id', 'proj_name', 'proj_desc']
       }]
     }]
   })
@@ -35,28 +35,71 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   models.Animal.findById(req.params.id, {
     include: [{
-      model: models.Species
+      model: models.Species,
+      attributes: ['id', 'species_name', 'subspecies', 'common_name']
     }, {
       model: models.Mark
     }, {
       model: models.Encounter,
-      attributes: {exclude: 'location_id'}
+      attributes: ['id', 'age', 'status', 'source', 'enc_date', 'comments'],
+      include: [{
+        model: models.Location,
+        attributes: ['id', 'loc_name', 'loc_lat', 'loc_lon'] // TODO: once mountain range and hunt unit data is incorporated include those data here.
+      }, {
+        model: models.Project,
+        attributes: ['id', 'proj_name', 'proj_desc']
+      }]
     }]
   })
-  .then(animal => {
-    res.status(200).json(animal);
-  });
+    .then(animal => {
+      res.status(200).json(animal);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 // CREATE (post) - create a new animal
 router.post('/', (req, res) => {
   models.Animal.create(req.body)
     .then(animal => {
-      res.status(200).json(animal);
+      res.status(200).json({
+        status: 'added animal to database',
+        data: 'localhost:8080' + '/animals/' + animal.id
+      });
     })
     .catch(err => {
       res.status(500).json(err);
     })
+});
+
+// UPDATE (patch/put) - edit an existing animal
+router.put('/:id', (req, res) => {
+  models.Animal.update(req.body, {
+    where: { id: req.params.id },
+    returning: true
+  })
+    .then(animal => {
+      res.status(200).json(animal);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+// DELETE (destroy) - delete an existing animal
+router.delete('/:id', (req, res) => {
+  models.Animal.destroy({
+    where: { id: req.params.id }
+  })
+    .then(animal => {
+      res.status(200).json({
+        status: 'deleted animal from database'
+      });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
